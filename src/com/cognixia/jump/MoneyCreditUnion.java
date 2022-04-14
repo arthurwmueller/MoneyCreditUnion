af63efafefec4b3fe3cf1963c729686f93b1adb9
unknown <awm21@pitt.edu>
2022-04-13 22:04:34 -0400

    first loop

--- src/com/cognixia/jump/MoneyCreditUnion.java
+++ src/com/cognixia/jump/MoneyCreditUnion.java
@@ -5,14 +5,31 @@ import java.util.Scanner;
 public class MoneyCreditUnion {
 
 	public static void main(String[] args) {
-		
+
 		Scanner input = new Scanner(System.in);
-		
+
 		System.out.println("Welcome to Money Credit Union\n");
-		
+
+		boolean loop = true;
+
+		while (loop) {
+			System.out.println("\nPlease make a selection");
+			System.out.println("1. Login");
+			System.out.println("2. Create an account");
+			System.out.println("3. Exit\n");
+			int selection = input.nextInt();
+			if (selection == 1) {
+
+			} else if (selection == 2) {
+
+			} else if (selection == 3) {
+				loop = false;
+			}
+		}
+
 		input.close();
-		
-		System.out.println("Goodbye");
+
+		System.out.println("\nGoodbye");
 
 	}
 
