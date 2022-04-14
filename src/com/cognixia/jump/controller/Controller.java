1f733cf419cf981fc13a6c7e70ae03fb65c674d2
unknown <awm21@pitt.edu>
2022-04-14 08:19:55 -0400

    added set password

--- src/com/cognixia/jump/controller/Controller.java
+++ src/com/cognixia/jump/controller/Controller.java
@@ -1,7 +1,7 @@
 package com.cognixia.jump.controller;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import com.cognixia.jump.model.Account;
@@ -10,7 +10,7 @@ public class Controller {
 
 	static Scanner input = new Scanner(System.in);
 
-	static Map<String, String> accounts = new HashMap<String, String>();
+	static List<Account> accounts = new ArrayList<Account>();
 
 	public static void createAccount() {
 		Account newAccount = new Account();
@@ -19,13 +19,26 @@ public class Controller {
 			System.out.println("/nPlease enter a username");
 			String newUsername = input.nextLine();
 			checkUsername = false;
-			for (String username : accounts.keySet()) {
-				if (newUsername == username) {
+			for (Account account : accounts) {
+				if (newUsername == account.getUsername()) {
 					checkUsername = true;
 				}
 			}
 			if (!checkUsername) {
 				newAccount.setUsername(newUsername);
+			}
+		}
+		boolean checkPassword = true;
+		while (checkPassword) {
+			System.out.println("\nPlease enter a pin");
+			String pin1 = input.nextLine();
+			System.out.println("\nPlease re-enter your pin");
+			String pin2 = input.nextLine();
+			if (pin1.equals(pin2)) {
+				checkPassword = false;
+				newAccount.setPin(pin1);
+			} else {
+				System.out.println("\nPins do not match try again\n");
 			}
 		}
 	}
