1c57f983c1613dfd4e9a0efe6175018f0c0fa1be
unknown <awm21@pitt.edu>
2022-04-13 21:30:28 -0400

    project initialized

---
+++ src/com/cognixia/jump/MoneyCreditUnion.java
@@ -0,0 +1,15 @@
+package com.cognixia.jump;
+
+import java.util.Scanner;
+
+public class MoneyCreditUnion {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		
+
+	}
+
+}
